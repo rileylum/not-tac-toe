393bbc16fb16c1988c4fca885fb9c1c7c7db9f83
Riley Lum <rileylum@adam.com.au>
2021-11-04 20:56:37 +1030

    update Board component

--- client/src/Board.js
+++ client/src/Board.js
@@ -1,27 +1,18 @@
 import React from "react";
 import Square from './Square'
+import './Board.css'
 
-function Board({gameBoard, clickHandler}) {
+function Board({gameBoard, boardSize, clickHandler}) {
+
+    const gridStyle = {gridTemplateColumns: `repeat(${boardSize}, 50px)`, gridTemplateRows: `repeat(${boardSize}, 50px)`}
+
     return (
-    <table>
-        <tbody>
-            <tr>
-                <Square id={0} value={gameBoard[0]} clickHandler={clickHandler} />
-                <Square id={1} value={gameBoard[1]} clickHandler={clickHandler} />
-                <Square id={2} value={gameBoard[2]} clickHandler={clickHandler} />
-            </tr>
-            <tr>
-                <Square id={3} value={gameBoard[3]} clickHandler={clickHandler} />
-                <Square id={4} value={gameBoard[4]} clickHandler={clickHandler} />
-                <Square id={5} value={gameBoard[5]} clickHandler={clickHandler} />
-            </tr>
-            <tr>
-                <Square id={6} value={gameBoard[6]} clickHandler={clickHandler} />
-                <Square id={7} value={gameBoard[7]} clickHandler={clickHandler} />
-                <Square id={8} value={gameBoard[8]} clickHandler={clickHandler} />
-            </tr>
-        </tbody>
-    </table>
+    <div className="Board" style={gridStyle}>
+        {gameBoard.map((cell, idx) => {
+            return (<Square id={idx} value={cell} clickHandler={clickHandler} />)
+        })}
+        
+    </div>
     );
 }
 
