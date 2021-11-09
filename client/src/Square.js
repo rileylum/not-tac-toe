c97611a042e77de6a155a2705dfd49d38e245391
Riley Lum <rileylum@adam.com.au>
2021-11-09 17:42:11 +1030

    add board styling

--- client/src/Square.js
+++ client/src/Square.js
@@ -4,7 +4,7 @@ import './Square.css';
 
 function Square({board_id, cell_id, value, clickHandler}) {
     return (
-        <button onClick={() => clickHandler(board_id, cell_id)} className="Square">{value ? "X" : "O"}</button>
+        <button onClick={() => clickHandler(board_id, cell_id)} className="Square">{value ? "X" : ""}</button>
     )
 }
 
